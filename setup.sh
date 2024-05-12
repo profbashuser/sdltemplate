c3f2d2dc5872a5ed97b05b1459d7f34b1b7f6731
Passkii <neopetersson5@gmail.com>
2024-05-12 13:11:32 +0200

    Add run target

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 git submodule init && git submodule update # Get and update submodules (SDL)
 mkdir -p build && cd build # Make and cd into the directory
 cmake .. && cd .. # Create the build structure
-cmake --build build && .'./build/Debug/CSDLBoiled.exe' # Build and run
+cmake --build build --target run
