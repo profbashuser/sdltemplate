3bf97f648550457e668d1057d44006bd4201b04f
Passkii <neopetersson5@gmail.com>
2024-05-12 13:14:14 +0200

    mf's when you merge the wrong thing

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,7 @@
 #include <SDL.h>
 #include <iostream>
 
-#include "WADLoader.h"
-
 int main()
 {
-    char* basePath = SDL_GetBasePath();
-    
-    if (basePath) {
-        std::string wadPath = std::string(basePath) + "../assets/DOOM1.wad";
-        WADLoader wadloader(wadPath);
-        wadloader.LoadWAD();
-        SDL_free(basePath);
-    } else {
-        std::cout << "Error getting base path: " << SDL_GetError() << std::endl;
-    }
-
     return 0;
 }
